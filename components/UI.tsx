d3f3a7b92394278e1afd2ceab96cbe901a036734
Namkyung <srirachacho@gmail.com>
2022-09-12 22:25:45 +0900

    Feat: 공통 컴포넌트 스타일 추가

--- components/UI.tsx
+++ components/UI.tsx
@@ -1,11 +1,30 @@
 import styled from 'styled-components';
 
+const Container = styled.div`
+  padding: 64px 32px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  font-weight: bold;
+  margin: 10px auto 10px;
+`;
+
 const Button = styled.button`
-  padding: 2px 10px;
+  padding: 5px 10px;
   background-color: ${({ theme }) => theme.colors.blue};
   border-radius: 5px;
   border-style: none;
   color: ${({ theme }) => theme.colors.white};
+
+  &:hover {
+    opacity: 0.5;
+  }
 `;
 
-export { Button };
+export { Container, Title, Button };
